28b79ebe2db907e92c14653f12c9a2aa9797869b
Jorge <92949107+JorgeDella@users.noreply.github.com>
2023-09-28 22:04:39 +0200

    Codi modificat

--- src/controlador/Connect.java
+++ src/controlador/Connect.java
@@ -1,5 +1,22 @@
 package controlador;
 
 public class Connect {
-    private String driver="com.mysql.jdbc.Driver";
+        public static Connect Connection(){
+        //Atributos
+            String driver="com.mysql.jdbc.Driver";
+            String cadenaConnexio="jdbc:mysql://127.0.0.1/agenda";
+            String usr="root";
+            String psswd="";
+            Connect con = null;
+            
+        //Constructor
+        try{
+            Class.forName(driver);
+            con=DriverManager.getConnection(cadenaConnexio, usr, psswd);
+            //JOptionPane.showMessageDialog(null, "Se ha connectado con la BBDD exitosamente.");
+        } catch(Exception e){
+            JOptionPane.showMessageDialog(null, "No se ha podido establecer una connexion con la BBDD "+e.getMessage());
+        }
+        return con;
+    }
 }
